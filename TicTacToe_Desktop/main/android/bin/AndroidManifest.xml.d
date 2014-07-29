b27dd71716184bff89cc6ba94ee0a7e715d0781b
Anthony Siu <anthonysiu@gmail.com>
2014-07-28 19:28:54 -0500

    Added ads to tictactoe

---
+++ TicTacToe_Desktop/main/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\bin\AndroidManifest.xml : \
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\AndroidManifest.xml \
---
+++ TicTacToe_Desktop/main/android/bin/main-release-unsigned.apk.d
@@ -0,0 +1,7 @@
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\bin\main-release-unsigned.apk : \
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\bin\main.ap_ \
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\bin\classes.dex \
+C:\Eclipse\sdk\extras\google\google_play_services\libproject\google-play-services_lib\bin\classes.jar \
+C:\Eclipse\sdk\tools\support\annotations.jar \
+C:\Eclipse\sdk\extras\google\google_play_services\libproject\google-play-services_lib\libs\google-play-services.jar \
+c:\Git\ProcessingWorkspace\TicTacToe_Desktop\main\android\libs\processing-core.jar \
